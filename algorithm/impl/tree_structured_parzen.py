ba1ec7a045775927acbcae749d75d659b7221b71
alexanderandreev <sesh_1.0@niuitmo.ru>
2023-03-28 04:41:47 +0300

    add draft TreeStructuredParzen algorithm

--- algorithm/impl/tree_structured_parzen.py
+++ algorithm/impl/tree_structured_parzen.py
@@ -17,7 +17,8 @@ class TreeStructuredParzen(Algorithm):
 
     def __init__(self, min_update_size: int, max_backdoor_mask_len: int, max_queue_size: Int = None):
         super().__init__(min_update_size, max_queue_size)
-        self.max_backdoor_mask_len = max_backdoor_mask_len  # название так себе
+        self.max_backdoor_mask_len = max_backdoor_mask_len
+        # название так себе, возможно стоит использовать SearchSet TODO remove comment
         sampler: TPESampler = optuna.samplers.TPESampler(seed=42)
         self.study: Study = optuna.create_study(direction='minimize', sampler=sampler)
         self.trails_cache: list[int] = []
@@ -25,24 +26,32 @@ class TreeStructuredParzen(Algorithm):
 
     def get_backdoor(self) -> 'Backdoor':
         trail: Trial = self.study.ask()
+        # вернуть trial, с помощью которого можно сгенерировать значения TODO remove comment
         backdoor: Backdoor = Backdoor(from_vars=Interval(start=1, length=self.max_backdoor_mask_len).variables())
         mask: List[int] = backdoor.get_mask()
         for i in range(self.max_backdoor_mask_len):
             take_x_i = trail.suggest_int(f'x{i}', 0, 1, step=1)
             mask[i] = take_x_i
+
         backdoor.set_trial_number(trail.number)
+        # необходимо запомнить trail.number, чтобы потом вызвать tell на нем TODO remove comment
         return backdoor._set_mask(mask)
+        # метод приватный, но кажется по другому нельзя TODO remove comment
 
 
     def update(self, vector: 'Vector', *points: 'Point') -> 'Vector':
         for point in points:
             trail_number: int = point.backdoor.trial_number
             if trail_number in self.used:
+                # некоторые значения могут доставаться из кеша и у них будет не верный trail_number
+                # поэтому приходится их пропускать TODO remove comment
                 print(f"skipp cached value {trail_number}")
                 continue
             self.study.tell(trail_number, point.value())
+            # сказать оптимизатору, что на trail_number достигается значение point.value() TODO remove comment
             self.used.add(trail_number)
             vector.append(point)
+            # вероятно нам не нужны все точки. Можно взять top n TODO remove comment
         self.trails_cache = []
         return vector
 
