7fe363484fbd1cc33f515c896fc1a7a9bb6b66c2
alexanderandreev <sesh_1.0@niuitmo.ru>
2023-03-31 03:10:30 +0300

    add draft TreeStructuredParzen algorithm

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from core.impl import Optimize
 from core.module.comparator import MinValueMaxSize
 from core.module.limitation import WallTime
 from core.module.sampling import Const
-from core.module.space import InputSet
+from core.module.space import InputSet, SearchSet
 from executor.impl import ProcessExecutor
-from function.impl import InverseBackdoorSets
-from function.module.measure import SolvingTime
+from function.impl import InverseBackdoorSets, RhoFunction
+from function.module.measure import SolvingTime, Propagations
 from function.module.solver import pysat
+from instance import Instance
 from instance.impl import StreamCipher
 from instance.module.encoding import CNF
 from instance.module.variables import Interval
@@ -21,22 +22,24 @@ if __name__ == '__main__':
 
     logs_path = root_path.to_path('logs', 'test')
     solution = Optimize(
-        space=InputSet(),
-        executor=ProcessExecutor(max_workers=32),
-        sampling=Const(size=1000, split_into=200),
-        instance=StreamCipher(
-            encoding=CNF(from_file=cnf_file),
-            input_set=Interval(start=1, length=64),
-            output_set=Interval(start=14375, length=64)
+        space=SearchSet(
+            by_mask=[],
+            variables=Interval(start=1, length=150)
         ),
-        function=InverseBackdoorSets(
-            measure=SolvingTime(budget=500),
+        executor=ProcessExecutor(max_workers=16),
+        sampling=Const(size=16384, split_into=4096),
+        instance=Instance(
+            encoding=CNF(from_file=cnf_file)
+        ),
+        function=RhoFunction(
+            penalty_power=2 ** 30,
+            measure=Propagations(),
             solver=pysat.Glucose3()
         ),
-        algorithm=TreeStructuredParzen(min_update_size=6, max_backdoor_mask_len=64),
+        algorithm=TreeStructuredParzen(min_update_size=6, max_backdoor_mask_len=150),
         comparator=MinValueMaxSize(),
         logger=OptimizeLogger(logs_path),
-        limitation=WallTime(from_string='00:11:00'),
+        limitation=WallTime(from_string='11:00:00'),
     ).launch()
 
     for point in solution:
