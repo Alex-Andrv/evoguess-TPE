7622beb98988ffe277187de3616bb987cb287b9b
alexanderandreev <sesh_1.0@niuitmo.ru>
2023-04-29 16:15:04 +0300

    SOS,GOD BLESS ME

--- examples/comb_example_2.py
+++ examples/comb_example_2.py
@@ -15,7 +15,7 @@ from typings.work_path import WorkPath
 
 if __name__ == '__main__':
     str_backdoors = [
-        '2083 2652 2653 2681 2722 2963 3409 3693 3844 5243 6297'
+        '5 67 342 618 669 738 1823 2750 2879 2986'
     ]
     backdoors = [
         make_backdoor(Indexes(from_string=str_vars))
@@ -24,7 +24,7 @@ if __name__ == '__main__':
 
     root_path = WorkPath('examples')
     data_path = root_path.to_path('data')
-    cnf_file = data_path.to_file('BvP_9_4.cnf')
+    cnf_file = data_path.to_file('dvw.cnf')
     logs_path = root_path.to_path('logs', 'sgen_150_comb')
     combine = Combine(
         instance=Instance(
